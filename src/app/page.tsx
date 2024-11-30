f48e2ca172fa963aad4a978a70f0ac49de74cc9f
KevinDanielDev <k4vind@gmail.com>
2024-11-30 12:24:05 -0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <span className="text-center text-5xl">Hello World</span>
+      </main>
+  );
+}
