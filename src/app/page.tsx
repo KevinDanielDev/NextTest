6b1157abb9a6287726c6d05a54f42fe350b78826
KevinDanielDev <k4vind@gmail.com>
2024-11-30 16:00:37 -0500

    feat: 🎸 New Page Welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <span className="text-center text-5xl">Hello World</span>
-      </main>
-  );
+  redirect("/welcome");
 }
